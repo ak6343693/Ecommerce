183d335cacc4d71b228226b1ca03fcae00cab4e8
ak6343693 <arunnokhwallaptop.com>
2025-04-30 11:37:16 +0530

    update

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDb = require('./database')
+const Product = require('./modals/product')
+const Customer = require('./modals/customer')
+const upload = require('./config/multer')
+const app = express()
+connectDb()
+app.use(express.json())
+
+app.post("/addproduct",upload.single('productImage'), async (req, res) => {
+    try {
+        const {productName,rating,price,discount}=req.body
+        const productImage=req.file.path
+        const products = await new Product({productName,rating,price,discount,productImage});
+        await products.save()
+        res.status(200).json({ message: "Product inserted successfully", products });
+    } catch (err) {
+        res.status(400).json({ error: err.message, message:'error in product adding'});
+    }
+});
+
+app.listen(5000,()=>console.log('server running on localhost 5000'))
